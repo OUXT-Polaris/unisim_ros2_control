a66a8a2dd7405a6f61357457b6274bbe91d4a485
Masaya Kataoka <ms.kataoka@gmail.com>
2021-10-03 22:17:56 +0900

    add comment
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- include/unisim_ros2_control/unisim_client.hpp
+++ include/unisim_ros2_control/unisim_client.hpp
@@ -41,8 +41,11 @@ public:
     configure(address, static_cast<std::uint16_t>(port));
   }
 
+/**
+ * @brief dispatch macro for the non-parameter post request.
+ */
 #define DISPATCH(NAME)             \
-  decltype(auto) NAME()            \
+  decltype(auto) NAME##Request()   \
   {                                \
     assert(api_ptr_);              \
     return api_ptr_->NAME##Post(); \
@@ -50,22 +53,28 @@ public:
   DISPATCH(stop)
 #undef DISPATCH
 
-#define DISPATCH(NAME, REQUEST_TYPE)                         \
-  decltype(auto) NAME(std::shared_ptr<REQUEST_TYPE> request) \
-  {                                                          \
-    assert(api_ptr_);                                        \
-    return api_ptr_->NAME##Post(request);                    \
+/**
+ * @brief dispatch macro for the required post request.
+ */
+#define DISPATCH(NAME, REQUEST_TYPE)                                  \
+  decltype(auto) NAME##Request(std::shared_ptr<REQUEST_TYPE> request) \
+  {                                                                   \
+    assert(api_ptr_);                                                 \
+    return api_ptr_->NAME##Post(request);                             \
   }
 
   DISPATCH(spawn, io::swagger::client::api::SpawnRequest)
   DISPATCH(start, io::swagger::client::api::StartSimulationRequest)
 #undef DISPATCH
 
-#define DISPATCH(NAME, REQUEST_TYPE)                                          \
-  decltype(auto) NAME(boost::optional<std::shared_ptr<REQUEST_TYPE>> request) \
-  {                                                                           \
-    assert(api_ptr_);                                                         \
-    return api_ptr_->NAME##Post(request);                                     \
+/**
+ * @brief dispatch macro for the non-required post request.
+ */
+#define DISPATCH(NAME, REQUEST_TYPE)                                                   \
+  decltype(auto) NAME##Request(boost::optional<std::shared_ptr<REQUEST_TYPE>> request) \
+  {                                                                                    \
+    assert(api_ptr_);                                                                  \
+    return api_ptr_->NAME##Post(request);                                              \
   }
 #undef DISPATCH
 
